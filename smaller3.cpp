f4e827ce92fc3da0c3562c2150c9e3bf2f30afcf
khadim24 <50873881+khadim24@users.noreply.github.com>
2024-01-30 19:05:43 -0500

    Add files via upload

---
+++ smaller3.cpp
@@ -0,0 +1,33 @@
+/*
+Author: Khadim Thiam
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab1A
+
+This program finds the smaller of the two integers
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int one;
+    int two;
+    int three;
+    cout << "Enter the first number: ";
+    cin >> one;
+    cout << "Enter the second number: ";
+    cin >> two;
+    cout << "Enter the third number: ";
+    cin >> three;
+    if(one < two && three){
+        cout << "The smaller of the three is " << one;
+    }
+    if(two < one && three){
+        cout << "The smaller of the three is " << two;
+    }
+    if(three < one && two){
+        cout << "The smaller of the three is " << three;
+    }
+    return 0;
+}
+//done
