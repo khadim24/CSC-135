3a81d2a50a850f74bad77215d6f5096f9389c1e2
khadim24 <50873881+khadim24@users.noreply.github.com>
2024-01-30 19:55:01 -0500

    Update leap.cpp

--- leap.cpp
+++ leap.cpp
@@ -5,23 +5,36 @@ Course: CSCI-135
 Instructor: Tong Yi
 Assignment: Lab1A
 
-This program check if a year is a leap year
+This program check how many months is in a month
 */
 #include <iostream>
 using namespace std;
 
 int main(){
     int year;
+    int month;
+    int days;
 
     cout << "Enter a year: ";
     cin >> year;
+    cout << "Enter a month: ";
+    cin >> month;
 
     // Check if it's a leap year or not
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-        cout << year << " is a Leap year.";
+    if (month == 2) {
+        // February has 28 or 29 days depending on whether it's a leap year
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            days = 29;
+        } else {
+            days = 28;
+        }
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        // April, June, September, and November have 30 days
+        days = 30;
     } else {
-        cout << year << " is a Common year.";
+        // All other months have 31 days
+        days = 31;
     }
-
+    cout << days << " days";
     return 0;
 }
